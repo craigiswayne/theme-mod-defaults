7d96be6372d790a1ca12ef83e48a4395dfc995ba
Craig Wayne <craigiswayne@gmail.com>
2018-09-09 20:10:44 +0200

    Fixes as per phpcbf

--- theme-mod-defaults.php
+++ theme-mod-defaults.php
@@ -28,17 +28,17 @@ const CONFIG_FILENAME = 'theme_mod.json';
  * @return false|string     False on failure. Path string on success.
  */
 function _locate_config_file() {
-	
+
 	if ( ! defined( 'STYLESHEETPATH' ) ) {
 		_doing_it_wrong( __FUNCTION__, 'This function must be called after the STYLEHSEETPATH constant has been defined', VERSION );
 		return false;
 	}
-	
+
 	$location = wp_cache_get( __FUNCTION__ ) ?: false;
-	
+
 	$path = STYLESHEETPATH;
-	
-	while ( $path . '/' !== dirname( ABSPATH ) && false === $location ) {
+
+	while ( dirname( ABSPATH ) !== $path . '/' && false === $location ) {
 		$_location = $path . '/' . CONFIG_FILENAME;
 		if ( ! file_exists( $_location ) ) {
 			$path = dirname( $path );
@@ -47,36 +47,38 @@ function _locate_config_file() {
 		$location = $_location;
 		wp_cache_set( __FUNCTION__, $location, __NAMESPACE__ );
 	}
-	
+
 	return $location;
 }
 
 /**
- * @return false|\stdClass     False on failure. JSON Decoded contents of json file
+ * Fetches the json config as an associative array
+ *
+ * @return false|array     False on failure
  */
 function json_config() {
-	
+
 	$json = wp_cache_get( __FUNCTION__, __NAMESPACE__ ) ?: false;
-	
+
 	if ( $json ) {
 		return $json;
 	}
-	
+
 	$config_file_path = _locate_config_file();
-	
+
 	if ( ! $config_file_path ) {
 		return false;
 	}
-	
+
 	$json = json_decode( file_get_contents( $config_file_path ), true ) ?: false;
-	
+
 	if ( $json ) {
 		wp_cache_set( __FUNCTION__, $json, __NAMESPACE__ );
 	} else {
 		error_log( __FUNCTION__ . ': Invalid JSON Format.' );
 		error_log( __FUNCTION__ . ": $config_file_path" );
 	}
-	
+
 	return $json;
 }
 
@@ -90,22 +92,30 @@ function _has_config() {
 	if ( ! json_config() ) {
 		return false;
 	}
-	
+
 	return true;
 }
 
+/**
+ * This is the entry point for this plugin.
+ * Sort of a controller, checking if a config exists/valid
+ *
+ * @return bool
+ */
 function init() {
-	
+
 	if ( ! _has_config() ) {
 		return false;
 	}
-	
+
 	$json_config = json_config();
-	
+
 	foreach ( $json_config as $setting_name => $default_value ) {
-		add_filter( "theme_mod_{$setting_name}", function ( $value ) use ( $json_config, $setting_name ) {
-			return false === $value ? ( $json_config[ $setting_name ] ?? $value ) : $value;
-		}, FILTER_PRIORITY );
+		add_filter(
+			"theme_mod_{$setting_name}", function ( $value ) use ( $json_config, $setting_name ) {
+				return false === $value ? ( $json_config[ $setting_name ] ?? $value ) : $value;
+			}, FILTER_PRIORITY
+		);
 	}
 }
 
